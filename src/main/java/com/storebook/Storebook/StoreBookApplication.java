e108a70952f337e43193225ae597204a5bc83b4d
Syavka <svatikap100@gmail.com>
2019-09-03 16:27:42 +0300

    initial commit

---
+++ src/main/java/com/storebook/Storebook/StoreBookApplication.java
@@ -0,0 +1,13 @@
+package com.storebook.Storebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBookApplication.class, args);
+	}
+
+}

b47b54fe09107ee93d85e5f08d2fda4605799690
maxcxam <maxcxam1979@gmail.com>
2019-09-06 08:41:00 +0300

    Add hibernate config and media Entity

---
+++ src/main/java/models/Media.java
@@ -0,0 +1,64 @@
+package models;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "media")
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private int id;
+
+    @Column(name = "path")
+    private String path;
+
+    @Column(name = "size")
+    private BigInteger size;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public BigInteger getSize() {
+        return size;
+    }
+
+    public void setSize(BigInteger size) {
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @OneToOne(mappedBy = "media")
+    private User user;
+
+    @OneToOne(mappedBy = "media")
+    private Author author;
+
+    @OneToOne(mappedBy = "media")
+    private Category category;
+
+    @OneToOne(mappedBy = "media")
+    private PaymentMethod paymentMethod;
+
+    @OneToOne(mappedBy = "media")
+    private Product product;
+
+    public Media(){}
+    public Media(String path, BigInteger size) {
+        this.path = path;
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return path;
+    }
+}

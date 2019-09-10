dd3e5062e08ddc8b36dff644bfccaa158afce9b8
ViktorMarynych <mrvikmar@gmail.com>
2019-09-11 00:00:23 +0300

    додав поля в клас Author

--- src/main/java/com/storebook/Storebook/models/Author.java
+++ src/main/java/com/storebook/Storebook/models/Author.java
@@ -1,4 +1,50 @@
 package com.storebook.Storebook.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "authors")
+
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    public Author() {
+    }
+
+    public Author(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
 }

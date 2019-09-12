bc0dfe336a6d167bdae1e2ed8902c635c097b819
ViktorMarynych <mrvikmar@gmail.com>
2019-09-12 14:56:53 +0300

    add entity Users

--- src/main/java/com/storebook/Storebook/models/User.java
+++ src/main/java/com/storebook/Storebook/models/User.java
@@ -1,4 +1,57 @@
 package com.storebook.Storebook.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String surname, String phone, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getSurname() {return surname;}
+    public void setSurname(String surname) {this.surname = surname;}
+
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public int getId() {return id;}
 }
+
+

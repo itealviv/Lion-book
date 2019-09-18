467c1c189aeada0bb04269d70920e92c878d90ea
maxcxam <maxcxam1979@gmail.com>
2019-09-18 23:32:40 +0300

    Add crossorigin headers for localhost

--- src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
+++ src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
@@ -5,6 +5,9 @@ import com.storebook.Storebook.models.Product;
 import com.storebook.Storebook.services.AuthorService;
 import com.storebook.Storebook.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +25,12 @@ public class AuthorController {
 
 
     @GetMapping("/authors")
-    public List<Author> getAuthors() {return authorService.getAllAuthors();}
+    public ResponseEntity<List<Author>>  getAuthors() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+
+        return new ResponseEntity<List<Author>>(authorService.getAllAuthors(),headers, HttpStatus.OK);
+    }
 
 
     @GetMapping("/author/{id}")

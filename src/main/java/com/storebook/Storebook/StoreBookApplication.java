8685f79bdda084479c5fdad35cf79e6f7304d2bb
maxcxam <maxcxam1979@gmail.com>
2019-09-18 23:50:11 +0300

    Add crossorigin headers for localhost

--- src/main/java/com/storebook/Storebook/StoreBookApplication.java
+++ src/main/java/com/storebook/Storebook/StoreBookApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("com.storebook.Storebook.models")
@@ -12,5 +14,16 @@ public class StoreBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreBookApplication.class, args);
 
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/authors").allowedOrigins("http://store-book.tk");
+				registry.addMapping("/author").allowedOrigins("http://store-book.tk");
+				registry.addMapping("/author/{id}/books").allowedOrigins("http://store-book.tk");
+			}
+		};
 	}
 }

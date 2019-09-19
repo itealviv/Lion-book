381edd2ecd1bf59b0bc85872eb23ae4633ff3e9b
Maxcxam <maxcxam1979@gmail.com>
2019-09-19 10:13:57 +0300

    cors policy bugfix

--- src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
+++ src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
@@ -5,14 +5,14 @@ import com.storebook.Storebook.models.Product;
 import com.storebook.Storebook.services.AuthorService;
 import com.storebook.Storebook.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+@CrossOrigin
 @RestController
 public class AuthorController {
     @Autowired
@@ -21,12 +21,11 @@ public class AuthorController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/authors")
-    public ResponseEntity<List<Author>>  getAuthors() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Origin", "*");
 
-        return new ResponseEntity<List<Author>>(authorService.getAllAuthors(),headers, HttpStatus.OK);
+
+    @GetMapping("/authors")
+    public List<Author>  getAuthors() {
+        return authorService.getAllAuthors();
     }
 
 

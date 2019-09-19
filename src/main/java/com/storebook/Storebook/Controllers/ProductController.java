3dbf5cf480195507b870609b34322fbb0eeba171
Syavka <svatikap100@gmail.com>
2019-09-19 22:21:59 +0300

    add delete endpoint to products controller

--- src/main/java/com/storebook/Storebook/Controllers/ProductController.java
+++ src/main/java/com/storebook/Storebook/Controllers/ProductController.java
@@ -3,10 +3,7 @@ package com.storebook.Storebook.Controllers;
 import com.storebook.Storebook.models.Product;
 import com.storebook.Storebook.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin
@@ -28,6 +25,11 @@ public class ProductController {
     @GetMapping("/product/{id}/author")
     public Product getProductAuthorById(@PathVariable int id) {
         return productService.getProduct(id);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        //do something here
     }
 
 

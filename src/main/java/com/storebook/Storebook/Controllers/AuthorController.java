44a2b1af7c49a0861c39418142212589b5578b05
Maxcxam <maxcxam1979@gmail.com>
2019-09-19 10:08:13 +0300

    cors policy bugfix

--- src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
+++ src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin
 @RestController
 public class AuthorController {
     @Autowired
@@ -23,14 +21,18 @@ public class AuthorController {
     @Autowired
     private ProductService productService;
 
-    @CrossOrigin
     @GetMapping("/authors")
-    public List<Author>  getAuthors() {
-        return authorService.getAllAuthors();
+    public ResponseEntity<List<Author>>  getAuthors() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+
+        return new ResponseEntity<List<Author>>(authorService.getAllAuthors(),headers, HttpStatus.OK);
     }
+
 
     @GetMapping("/author/{id}")
     public Author getAuthor(@PathVariable int id) { return authorService.getById(id);}
+
 
     @GetMapping("/author/{id}/books")
     public List<Product> getProducts(@PathVariable int id) { return productService.findAllByAuthor(id);}

32086640f1e77fe647035a4b2783b1186798ad6e
Maxcxam <maxcxam1979@gmail.com>
2019-09-19 09:55:32 +0300

    cors policy bugfix

--- src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
+++ src/main/java/com/storebook/Storebook/Controllers/AuthorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 public class AuthorController {
     @Autowired

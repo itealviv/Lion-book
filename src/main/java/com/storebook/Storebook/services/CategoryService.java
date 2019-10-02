a4c3f13cf2cad050d460b470494e7dd92284e811
ViktorMarynych <mrvikmar@gmail.com>
2019-10-02 19:47:55 +0300

    in CategoryService

--- src/main/java/com/storebook/Storebook/services/CategoryService.java
+++ src/main/java/com/storebook/Storebook/services/CategoryService.java
@@ -9,11 +9,17 @@ import java.util.List;
 
 @Service
 public class CategoryService {
-    @Autowired
+
     private CategoryRepository categoryRepository;
+    
+@Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll(); }
 
-    public List<Category> getAllCategories() { return categoryRepository.findAll(); }
-
-    public Category getById(int id) { return categoryRepository.getOne(id); }
+    public Category getById(int id) {
+        return categoryRepository.getOne(id); }
 }
